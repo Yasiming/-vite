12fdb3c7da985a35ccd35801add3ae216c47ea02
hutianxin <1635423653@qq.com>
2022-08-22 22:12:28 +0800

    reactive

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,56 +1,32 @@
-<script setup lang="ts">
-import { Ref, ref, isRef, shallowRef, triggerRef, customRef } from "@vue/reactivity";
-
-let message: Ref<string> = ref("小满")
-let notRef: number = 1
-const changeMsg = () => {
-  message.value = 'change msg'
-  console.log(message);
-  console.log('isRef', isRef(message)); // true
-  console.log('isRef', isRef(notRef));  // false
-}
-
-let h_ShallowRef  = shallowRef({
-  name: "小满"
-})
-const changeShallowRef = () => {
-  //  响应式改变
-  // h_ShallowRef.value = {name:"大满"}
-  h_ShallowRef.value.name = "大满"
-  triggerRef(h_ShallowRef)
-}
-
-// 自定义Ref
-function MyRef<T>(value: T) {
-  return customRef((trank,trigger) => {
-    return {
-      get() {
-          trank()
-          return value
-      },
-      set(newVal: T) {
-          value = newVal
-          trigger()
-      }
-    }
-  })
-}
-let message2 = MyRef<string>("小满")
-const changeMsg2 = () => {
-  message2.value = "大满"
-}
-</script>
-
-<!--  -->
 <template>
   <div>
-    {{message}}
-    <button @click="changeMsg">改变</button>
-    {{h_ShallowRef}}
-    <button @click="changeShallowRef">shallowRef</button>
+    <div>arr {{message}}</div>
+    <div>obj: {{obj}}</div>
+    <div>person: {{person}}</div>
   </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { reactive } from "@vue/reactivity";
+
+let message = reactive([])
+let obj = reactive({
+  name:"sm"
+})
+
+type Person = {
+  list ?: number []
+}
+let person = reactive<Person>({
+  list: []
+})
+setTimeout(() => {
+  person.list = [1,2,3]
+  console.log(person);
+  
+}, 1000)
+</script>
+
+<style>
 
 </style>
